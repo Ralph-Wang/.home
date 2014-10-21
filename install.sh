e93edb8649f2049782bd95b3cf796eee021f63fb
Ralph-Wang <ralph_ld@aliyun.com>
2014-10-21 17:44:43 +0800

    添加 bin

--- install.sh
+++ install.sh
@@ -2,18 +2,17 @@
 
 
 # .bash_profile
-rm -f ~/.bash_profile
-ln -s ~/.home/bash_profile ~/.bash_profile
+ln -sf ~/.home/bash_profile ~/.bash_profile
 
 # .alias
-rm -f ~/.alias
-ln -s ~/.home/alias ~/.alias
+ln -sf ~/.home/alias ~/.alias
 
 # tmux config
-rm -f ~/.tmux.conf
-ln -s ~/.home/tmux.conf ~/.tmux.conf
+ln -sf ~/.home/tmux.conf ~/.tmux.conf
 
 # local file
-rm -f ~/.bash_profile.local
 touch ~/.home/bash_profile.local
-ln -s ~/.home/bash_profile.local ~/.bash_profile.local
+ln -sf ~/.home/bash_profile.local ~/.bash_profile.local
+
+# commands
+ln -sf ~/.home/bin ~/.bin
