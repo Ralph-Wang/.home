886225f71044f634308ff2dd8e14d868b9f7d2c2
Ralph Wang <ralph.wang@moblab.com>
2019-12-17 14:07:44 +0800

    use bash function instead of the file commmands

--- install.sh
+++ install.sh
@@ -13,12 +13,6 @@ ln -sf ~/.home/tmux.conf ~/.tmux.conf
 # local file
 touch ~/.home/bash_profile.local
 
-
-# commands
-/bin/rm -rf ~/.bin
-ln -sf ~/.home/bin ~/.bin
-
 source ~/.bash_profile
 
-echo "install over."
 echo "enjoy your terminal life."
